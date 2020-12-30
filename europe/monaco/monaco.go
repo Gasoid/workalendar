555d5b119600e72f5691c785fae8f0fbdfba9e98
Rinat Almakhov <gasoid@gmail.com>
2020-12-30 14:58:01 +0400

    Monaco #44

---
+++ europe/monaco/monaco.go
@@ -0,0 +1,53 @@
+package monaco
+
+import (
+	"fmt"
+	"time"
+
+	core "github.com/Gasoid/workalendar"
+)
+
+var (
+	holidays = core.Holidays{
+		"1/27":  core.Event("Saint Dévote's Day"),
+		"11/19": core.Event("H.S.H. the Sovereign Prince's Day"),
+	}
+	calendar = core.NewCalendar(
+		holidays,
+		core.WithNewYearDay(),
+		core.WithEasterMonday(),
+		core.WithAscension(),
+		core.WithWhitMonday(),
+		core.WithAllSaints(),
+		core.WithAssumption(),
+		core.WithCorpusChristi(),
+		core.WithImmaculateConception(),
+		core.WithLabourDay(),
+	)
+)
+
+func variableHolidays(date time.Time) core.Holidays {
+	return nil
+}
+
+//IsWorkingDay is inteded to check whether a day is working or not
+func IsWorkingDay(date time.Time) bool {
+	return calendar.IsWorkingDay(date)
+}
+
+//IsHoliday is inteded to check whether a day is holiday or not
+func IsHoliday(date time.Time) bool {
+	return calendar.IsHoliday(date)
+}
+
+//GetHoliday is inteded to check whether a day is holiday or not
+func GetHoliday(date time.Time) (*core.CalEvent, error) {
+	if !IsHoliday(date) {
+		return nil, fmt.Errorf("There is no holiday for %s", date)
+	}
+	holiday := calendar.GetHoliday(date)
+	if holiday == nil {
+		return nil, fmt.Errorf("There is no holiday for %s", date)
+	}
+	return calendar.GetHoliday(date), nil
+}
