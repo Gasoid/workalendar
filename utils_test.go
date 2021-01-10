376f2c6053f4d0c50d672aa75279fcabb8084151
Rinat Almakhov <gasoid@gmail.com>
2021-01-10 13:39:45 +0400

    adjusted

--- utils_test.go
+++ utils_test.go
@@ -30,21 +30,21 @@ func TestFindWorkingDay(t *testing.T) {
 	monday := jan10.AddDate(0, 0, 1)
 	jan9 := jan10.AddDate(0, 0, -1)
 	friday := time.Date(2021, time.January, 15, 0, 0, 0, 0, time.UTC)
-	assert.Equal(t, monday, FindWorkingDay(jan10))
-	assert.Equal(t, monday, FindWorkingDay(jan9))
-	assert.Equal(t, friday, FindWorkingDay(friday))
+	assert.Equal(t, monday.Format("2006/1/2"), FindWorkingDay(jan10).Format("2006/1/2"))
+	assert.Equal(t, monday.Format("2006/1/2"), FindWorkingDay(jan9).Format("2006/1/2"))
+	assert.Equal(t, friday.Format("2006/1/2"), FindWorkingDay(friday).Format("2006/1/2"))
 }
 
 func TestFirstMonday(t *testing.T) {
 	jan1 := time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC)
 	jan4 := time.Date(2021, time.January, 4, 0, 0, 0, 0, time.UTC)
 	feb1 := time.Date(2021, time.February, 1, 0, 0, 0, 0, time.UTC)
-	assert.Equal(t, jan4, FindFirstMonday(jan1))
-	assert.Equal(t, feb1, FindFirstMonday(feb1))
+	assert.Equal(t, jan4.Format("2006/1/2"), FindFirstMonday(jan1).Format("2006/1/2"))
+	assert.Equal(t, feb1.Format("2006/1/2"), FindFirstMonday(feb1).Format("2006/1/2"))
 }
 
 func TestLastMonday(t *testing.T) {
 	jan4 := time.Date(2021, time.January, 4, 0, 0, 0, 0, time.UTC)
 	jan25 := time.Date(2021, time.January, 25, 0, 0, 0, 0, time.UTC)
-	assert.Equal(t, jan25, FindLastMonday(jan4))
+	assert.Equal(t, jan25.Format("2006/1/2"), FindLastMonday(jan4).Format("2006/1/2"))
 }
