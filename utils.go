7815e6d197af3ad0658230acb5cd6abae6bd6dd6
Rinat Almakhov <gasoid@gmail.com>
2021-01-10 14:14:23 +0400

    hm, let do this

--- utils.go
+++ utils.go
@@ -15,8 +15,8 @@ const (
 
 //FindLastMonday returns the last monday of month
 func FindLastMonday(date time.Time) time.Time {
-	lastDay := time.Date(date.Year(), date.Month()+1, 0, 10, 0, 0, 0, time.UTC)
-	numDays := []int{-6, 0, -1, -2, -3, -4, -5}
+	lastDay := time.Date(date.Year(), date.Month()+1, 1, 1, 0, 0, 0, time.UTC)
+	numDays := []int{-6, -7, -1, -2, -3, -4, -5}
 	date = date.AddDate(0, 0, numDays[int(lastDay.Weekday())])
 	return date
 }
