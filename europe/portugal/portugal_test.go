4fbeda57dde08283e5a10bd8386e406e1f518a97
Rinat Almakhov <gasoid@gmail.com>
2021-01-31 23:32:25 +0400

    added tests

---
+++ europe/portugal/portugal_test.go
@@ -0,0 +1,37 @@
+package portugal_test
+
+import (
+	"testing"
+	"time"
+
+	country "github.com/Gasoid/workalendar/europe/portugal"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	holiday1   = time.Date(2020, time.April, 25, 0, 0, 0, 0, time.UTC)
+	holiday2   = time.Date(2020, time.June, 10, 0, 0, 0, 0, time.UTC)
+	notHoliday = time.Date(2020, time.May, 17, 0, 0, 0, 0, time.UTC)
+)
+
+func TestIsHoliday(t *testing.T) {
+	assert.True(t, country.IsHoliday(holiday1))
+	assert.True(t, country.IsHoliday(holiday2))
+}
+
+func TestGetHoliday(t *testing.T) {
+	holiday, err := country.GetHoliday(holiday1)
+	v := "Dia da Liberdade"
+	assert.NoError(t, err)
+	assert.Equal(t, v, holiday.Name)
+}
+
+func TestGetHoliday_NotHoliday(t *testing.T) {
+	_, err := country.GetHoliday(notHoliday)
+	assert.Error(t, err)
+}
+
+func TestIsWorkingDay(t *testing.T) {
+	assert.False(t, country.IsWorkingDay(holiday1))
+	assert.False(t, country.IsWorkingDay(holiday2))
+}
---
+++ europe/serbia/serbia_test.go
@@ -0,0 +1,37 @@
+package serbia_test
+
+import (
+	"testing"
+	"time"
+
+	country "github.com/Gasoid/workalendar/europe/serbia"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	holiday1   = time.Date(2020, time.February, 15, 0, 0, 0, 0, time.UTC)
+	holiday2   = time.Date(2020, time.November, 11, 0, 0, 0, 0, time.UTC)
+	notHoliday = time.Date(2020, time.May, 17, 0, 0, 0, 0, time.UTC)
+)
+
+func TestIsHoliday(t *testing.T) {
+	assert.True(t, country.IsHoliday(holiday1))
+	assert.True(t, country.IsHoliday(holiday2))
+}
+
+func TestGetHoliday(t *testing.T) {
+	holiday, err := country.GetHoliday(holiday1)
+	v := "Statehood Day"
+	assert.NoError(t, err)
+	assert.Equal(t, v, holiday.Name)
+}
+
+func TestGetHoliday_NotHoliday(t *testing.T) {
+	_, err := country.GetHoliday(notHoliday)
+	assert.Error(t, err)
+}
+
+func TestIsWorkingDay(t *testing.T) {
+	assert.False(t, country.IsWorkingDay(holiday1))
+	assert.False(t, country.IsWorkingDay(holiday2))
+}
